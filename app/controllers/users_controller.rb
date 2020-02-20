5b10228270a61b1b45a52807711e790b1c51523e
Thomas Gaston <thomasgaston11@gmail.com>
2020-02-20 09:37:22 -0700

    more stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,33 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :hair_color, :eye_color, :age, :alive)
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
   end
 
 end
