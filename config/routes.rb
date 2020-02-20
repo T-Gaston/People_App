95786b1d9dfc7c7ff7c94e98b3294984355c3a02
Thomas Gaston <thomasgaston11@gmail.com>
2020-02-19 17:02:40 -0700

    added show user and create new user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
